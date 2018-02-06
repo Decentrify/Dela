edc49d3587ec0c1d93f44fb82b72bd8076990b79
Alex Ormenisan <ormenisan.adrian@gmail.com>
2018-02-06 17:47:13 +0100

    client fixes

--- client/src/main/java/se/sics/silk/client/Launcher.java
+++ client/src/main/java/se/sics/silk/client/Launcher.java
@@ -3,7 +3,9 @@ package se.sics.silk.client;
 import com.google.common.base.Optional;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,7 @@ import se.sics.kompics.Channel;
 import se.sics.kompics.ClassMatchedHandler;
 import se.sics.kompics.Component;
 import se.sics.kompics.ComponentDefinition;
+import se.sics.kompics.ComponentProxy;
 import se.sics.kompics.Handler;
 import se.sics.kompics.Init;
 import se.sics.kompics.Kompics;
@@ -25,16 +28,22 @@ import se.sics.kompics.fsm.id.FSMIdentifierFactory;
 import se.sics.kompics.network.Network;
 import se.sics.kompics.timer.Timer;
 import se.sics.kompics.timer.java.JavaTimer;
+import se.sics.kompics.util.Identifier;
 import se.sics.ktoolbox.croupier.CroupierSerializerSetup;
 import se.sics.ktoolbox.gradient.GradientSerializerSetup;
 import se.sics.ktoolbox.netmngr.NetworkMngrSerializerSetup;
 import se.sics.ktoolbox.netmngr.event.NetMngrReady;
 import se.sics.ktoolbox.omngr.OMngrSerializerSetup;
+import se.sics.ktoolbox.util.config.options.BasicAddressBootstrapOption;
+import se.sics.ktoolbox.util.identifiable.BasicBuilders;
 import se.sics.ktoolbox.util.identifiable.BasicIdentifiers;
 import se.sics.ktoolbox.util.identifiable.IdentifierFactory;
 import se.sics.ktoolbox.util.identifiable.IdentifierRegistry;
+import se.sics.ktoolbox.util.identifiable.basic.IntIdFactory;
+import se.sics.ktoolbox.util.identifiable.overlay.OverlayId;
 import se.sics.ktoolbox.util.identifiable.overlay.OverlayIdFactory;
 import se.sics.ktoolbox.util.identifiable.overlay.OverlayRegistry;
+import se.sics.ktoolbox.util.managedStore.core.util.HashUtil;
 import se.sics.ktoolbox.util.network.KAddress;
 import se.sics.ktoolbox.util.setup.BasicSerializerSetup;
 import se.sics.ktoolbox.util.status.Status;
@@ -42,22 +51,34 @@ import se.sics.ktoolbox.util.status.StatusPort;
 import se.sics.ledbat.LedbatSerializerSetup;
 import se.sics.nat.mngr.SimpleNatMngrComp;
 import se.sics.nat.stun.StunSerializerSetup;
+import se.sics.nstream.StreamId;
 import se.sics.nstream.TorrentIds;
+import se.sics.nstream.hops.storage.disk.DiskEndpoint;
+import se.sics.nstream.hops.storage.disk.DiskResource;
 import se.sics.nstream.storage.durable.DStorageMngrComp;
 import se.sics.nstream.storage.durable.DStoragePort;
 import se.sics.nstream.storage.durable.DStreamControlPort;
+import se.sics.nstream.storage.durable.util.MyStream;
+import se.sics.nstream.storage.durable.util.StreamEndpoint;
+import se.sics.nstream.storage.durable.util.StreamResource;
+import se.sics.nstream.util.BlockDetails;
 import se.sics.silk.SystemOverlays;
+import se.sics.silk.TorrentIdHelper;
 import se.sics.silk.r2torrent.R2TorrentComp;
 import se.sics.silk.r2torrent.torrent.R1FileDownload;
 import se.sics.silk.r2torrent.torrent.R1FileUpload;
 import se.sics.silk.r2torrent.torrent.R1Torrent;
+import se.sics.silk.r2torrent.torrent.R1TorrentCtrlPort;
+import se.sics.silk.r2torrent.torrent.event.R1TorrentCtrlEvents;
+import se.sics.silk.r2torrent.torrent.util.R1FileMetadata;
+import se.sics.silk.r2torrent.torrent.util.R1TorrentDetails;
 import se.sics.silk.r2torrent.transfer.R1TransferLeecher;
 import se.sics.silk.r2torrent.transfer.R1TransferSeeder;
 import se.sics.silkold.torrentmngr.TorrentMngrFSM;
 
 public class Launcher extends ComponentDefinition {
 
-  private Logger LOG = LoggerFactory.getLogger(Launcher.class);
+  private static Logger LOG = LoggerFactory.getLogger(Launcher.class);
   private String logPrefix = "";
 
   //*****************************CONNECTIONS**********************************
@@ -78,38 +99,56 @@ public class Launcher extends ComponentDefinition {
     subscribe(handleStart, control);
     subscribe(handleNetReady, otherStatusPort);
 
-    clientSetup();
+    clientConfigSetup();
     systemSetup();
   }
 
-  private void clientSetup() {
-    //dela dir
-    String delaS = config().getValue("hops.library.disk.summary", String.class);
-    if (delaS == null) {
-      delaS = System.getProperty("user.home") + File.separator + ".dela" + File.separator + "library.summary";
-      Config.Builder cb = config().modify(id());
-      cb.setValue("hops.library.disk.summary", delaS);
-      ConfigUpdate cu = cb.finalise();
-      updateConfig(cu);
-    }
-    File dela = new File(delaS);
-    if (!dela.exists()) {
-      dela.getParentFile().mkdirs();
-    }
+  private void clientConfigSetup() {
     //ipv4
     System.setProperty("java.net.preferIPv4Stack", "true");
-    //config
-    Long seed = config().getValue("system.seed", Long.class);
+
+    //kompics config
+    Config.Builder cb = config().modify(id());
+    //
+    Long seed = config().getValue(ClientConfig.SYSTEM_SEED, Long.class);
     if (seed == null) {
       Random rand = new Random();
       seed = rand.nextLong();
-      Config.Builder cb = config().modify(id());
       cb.setValue("system.seed", seed);
-      ConfigUpdate cu = cb.finalise();
-      updateConfig(cu);
     }
+    //
+    String librarySummaryPath = config().getValue(ClientConfig.LIBRARY_SUMMARY, String.class);
+    if (librarySummaryPath == null) {
+      librarySummaryPath = System.getProperty("user.home") + File.separator + ".dela" + File.separator
+        + "library.summary";
+      cb.setValue(ClientConfig.LIBRARY_SUMMARY, librarySummaryPath);
+    }
+    File librarySummaryFile = new File(librarySummaryPath);
+    if (!librarySummaryFile.exists()) {
+      librarySummaryFile.getParentFile().mkdirs();
+      try {
+        if (!librarySummaryFile.createNewFile()) {
+          throw new RuntimeException("ups");
+        }
+      } catch (IOException ex) {
+        throw new RuntimeException("ups");
+      }
+    }
+    //
+    String libraryPath = config().getValue(ClientConfig.LIBRARY_PATH, String.class);
+    if (libraryPath == null) {
+      libraryPath = System.getProperty("user.home") + File.separator + ".dela" + File.separator + "library";
+      cb.setValue(ClientConfig.LIBRARY_SUMMARY, libraryPath);
+    }
+    File librarySummaryDir = new File(libraryPath);
+    if (!librarySummaryDir.exists()) {
+      librarySummaryDir.mkdirs();
+    }
+    //
+    ConfigUpdate cu = cb.finalise();
+    updateConfig(cu);
   }
-  
+
   private void systemSetup() {
     //identifier setup
     TorrentIds.registerDefaults(config().getValue("system.seed", Long.class));
@@ -173,12 +212,13 @@ public class Launcher extends ComponentDefinition {
 
         setStorageMngr();
         setTorrentMngr();
-
+        
         trigger(Start.event, storageMngrComp.control());
         trigger(Start.event, torrentMngrComp.control());
 
         LOG.info("{}dela started", logPrefix);
-        doCommand();
+        Driver driver = new Driver(proxy, config(), torrentMngrComp);
+        driver.doCommand();
       }
     };
 
@@ -196,7 +236,7 @@ public class Launcher extends ComponentDefinition {
     connect(torrentMngrComp.getNegative(DStoragePort.class), storageMngrComp.getPositive(DStoragePort.class),
       Channel.TWO_WAY);
   }
-
+  
   private static void setupFSM() throws FSMException {
     FSMIdentifierFactory fsmIdFactory = FSMIdentifierFactory.DEFAULT;
     fsmIdFactory.registerFSMDefId(TorrentMngrFSM.NAME);
@@ -205,7 +245,7 @@ public class Launcher extends ComponentDefinition {
     fsmIdFactory.registerFSMDefId(R1FileUpload.NAME);
     fsmIdFactory.registerFSMDefId(R1TransferSeeder.NAME);
     fsmIdFactory.registerFSMDefId(R1TransferLeecher.NAME);
-    
+
     Config.Impl config = (Config.Impl) Kompics.getConfig();
     Config.Builder builder = Kompics.getConfig().modify(UUID.randomUUID());
     builder.setValue(FSMIdentifierFactory.CONFIG_KEY, fsmIdFactory);
@@ -213,23 +253,8 @@ public class Launcher extends ComponentDefinition {
     Kompics.setConfig(config);
   }
 
-//  private void readCommand() {
-//    LOG.info("commands:");
-//    LOG.info("1) download:dataset - example: download:datasetA_1");
-//    LOG.info("waiting for command...");
-//    Scanner scanner = new Scanner(System.in);
-//    String input = scanner.nextLine();
-//    LOG.info("command:{}", input);
-//    if (input.startsWith("download")) {
-//      String dataset = input.substring(input.indexOf(":") + 1);
-//    }
-//  }
-
-  private void doCommand() {
-    String dataset = "datasetA_1";
-  }
-
   public static void main(String[] args) throws IOException, FSMException {
+    Launcher.LOG.info("config file:{}", System.getProperties().getProperty("config.file"));
     if (Kompics.isOn()) {
       Kompics.shutdown();
     }
@@ -239,6 +264,113 @@ public class Launcher extends ComponentDefinition {
       Kompics.waitForTermination();
     } catch (InterruptedException ex) {
       System.exit(1);
+    }
+  }
+
+  public static class ClientConfig {
+
+    public static final String SYSTEM_SEED = "system.seed";
+    public static final String DELA = "hops";
+    public static final String DELA_BOOTSTRAP = DELA + ".bootstrap";
+    public static final String LIBRARY_SUMMARY = DELA + ".library.disk.summary";
+    public static final String LIBRARY_PATH = DELA + ".library.path";
+    public static final String COMMAND = "command";
+    
+    public final static BasicAddressBootstrapOption delaBootstrap = new BasicAddressBootstrapOption(DELA_BOOTSTRAP);
+  }
+
+  public static class Driver {
+
+    final ComponentProxy proxy;
+    final Component torrentMngrComp;
+    final Config config;
+    OverlayId torrentId;
+    R1TorrentDetails torrentDetails;
+
+    public Driver(ComponentProxy proxy, Config config, Component torrentMngrComp) {
+      this.proxy = proxy;
+      this.torrentMngrComp = torrentMngrComp;
+      this.config = config;
+    }
+
+    private void torrentDetails(String torrentName) {
+      
+      torrentId = TorrentIds.torrentId(new BasicBuilders.IntBuilder(1));
+      torrentDetails = new R1TorrentDetails(HashUtil.getAlgName(HashUtil.SHA));
+      IntIdFactory intIdFactory = new IntIdFactory(new Random());
+
+      Identifier file1 = intIdFactory.id(new BasicBuilders.IntBuilder(1));
+      Identifier file2 = intIdFactory.id(new BasicBuilders.IntBuilder(2));
+      Identifier file3 = intIdFactory.id(new BasicBuilders.IntBuilder(3));
+      Identifier file4 = intIdFactory.id(new BasicBuilders.IntBuilder(4));
+      Identifier file5 = intIdFactory.id(new BasicBuilders.IntBuilder(5));
+
+      long fileLength = 1000 * 1000;
+      int pieceSize = 1024;
+      int nrPieces = 10;
+      int nrBlocks = 5;
+      BlockDetails defaultBlock = new BlockDetails(pieceSize * nrPieces, nrPieces, pieceSize, pieceSize);
+      R1FileMetadata fileMetadata = R1FileMetadata.instance(fileLength, defaultBlock);
+
+      torrentDetails.addMetadata(file1, fileMetadata);
+      torrentDetails.addMetadata(file2, fileMetadata);
+      torrentDetails.addMetadata(file3, fileMetadata);
+      torrentDetails.addMetadata(file4, fileMetadata);
+      torrentDetails.addMetadata(file5, fileMetadata);
+      
+      Identifier endpointId = intIdFactory.id(new BasicBuilders.IntBuilder(0));
+      StreamId streamId1 = TorrentIdHelper.streamId(endpointId, torrentId, file1);
+      StreamId streamId2 = TorrentIdHelper.streamId(endpointId, torrentId, file2);
+      StreamId streamId3 = TorrentIdHelper.streamId(endpointId, torrentId, file3);
+      StreamId streamId4 = TorrentIdHelper.streamId(endpointId, torrentId, file4);
+      StreamId streamId5 = TorrentIdHelper.streamId(endpointId, torrentId, file5);
+      
+      StreamEndpoint endpoint = new DiskEndpoint();
+      String torrentPath = config.getValue("LIBRARY_PATH", String.class) + File.separator + torrentName;
+      StreamResource resource1 = new DiskResource(torrentPath, "file1");
+      StreamResource resource2 = new DiskResource(torrentPath, "file2");
+      StreamResource resource3 = new DiskResource(torrentPath, "file3");
+      StreamResource resource4 = new DiskResource(torrentPath, "file4");
+      StreamResource resource5 = new DiskResource(torrentPath, "file5");
+      
+      MyStream stream1 = new MyStream(endpoint, resource1);
+      MyStream stream2 = new MyStream(endpoint, resource2);
+      MyStream stream3 = new MyStream(endpoint, resource3);
+      MyStream stream4 = new MyStream(endpoint, resource4);
+      MyStream stream5 = new MyStream(endpoint, resource5);
+      
+      torrentDetails.addStorage(file1, streamId1, stream1);
+      torrentDetails.addStorage(file2, streamId2, stream2);
+      torrentDetails.addStorage(file3, streamId3, stream3);
+      torrentDetails.addStorage(file4, streamId4, stream4);
+      torrentDetails.addStorage(file5, streamId5, stream5);
+    }
+
+    public void doCommand() {
+      String command = config.getValue(ClientConfig.COMMAND, String.class);
+      LOG.info("performing command:{}", command);
+      switch (command) {
+        case "upload":
+          upload();
+          break;
+        case "download":
+          download();
+          break;
+      }
+    }
+
+    public void upload() {
+      torrentDetails("upload");
+      R1TorrentCtrlEvents.Upload req = new R1TorrentCtrlEvents.Upload(torrentId, torrentDetails);
+      proxy.trigger(req, torrentMngrComp.getPositive(R1TorrentCtrlPort.class));
+    }
+
+    
+    private void download() {
+      torrentDetails("download");
+      Set<KAddress> bootstrap = new HashSet<>(ClientConfig.delaBootstrap.readValue(config).get());
+      R1TorrentCtrlEvents.Download req = new R1TorrentCtrlEvents.Download(torrentId, torrentDetails, bootstrap);
+      proxy.trigger(req, torrentMngrComp.getPositive(R1TorrentCtrlPort.class));
     }
   }
 }
